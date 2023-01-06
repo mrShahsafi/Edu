0532b4cb6cf794aa98fe2e5dd33280e4e84ce32f
mrshahsafi <xvstanxv@gmail.com>
2023-01-06 14:58:54 +0330

    adding response dir

---
+++ djnagoForFun/core/responses/__init__.py
@@ -0,0 +1,1 @@
+from .api_messages  import *
---
+++ djnagoForFun/core/responses/api_messages.py
@@ -0,0 +1,39 @@
+MESSAGE_CONTENT_EN = "message"
+ERROR_CONTENT_EN = "error"
+DATA_CONTENT_EN = "data"
+
+MESSAGE_CONTENT_DE = "Nachricht"
+ERROR_CONTENT_DE = "Fehler"
+DATA_CONTENT_DE = "Daten"
+
+EMAIL = "email"
+WHATSAPP = "whatsapp_number"
+PASS = "password"
+OLD_PASS = "old_password"
+NEW_PASS = "new_password"
+DETAIL = "detail"
+EXTRA = "extra"
+
+VALIDATION_ERR_EN = "Validation error"
+VALIDATION_ERR_DE = "Bestätigungsfehler"
+
+UNKNOWN_EN = "There is an unknown content, contact the administrator"
+UNKNOWN_DE = "Es gibt einen unbekannten Inhalt, wenden Sie sich an den Administrator"
+
+SERVER_ERROR_EN = "Your request cannot be completed, please try again later!"
+SERVER_ERROR_DE = (
+    "Deine Anfrage konnte nicht bearbeitet werden, bitte versuche es später erneut!"
+)
+
+ENTITY_DELETED_EN = "Resource deleted successfully"
+ENTITY_DELETED_DE = "Ressource erfolgreich gelöscht"
+
+REQUIRED_FIELD_EN = "This field is required"
+REQUIRED_FIELD_DE = "Dieses Feld ist obligatorisch!"
+
+INVALID_LINK_EN = "Link is not valid"
+INVALID_LINK_DE = "Link nicht gültig"
+
+EXPIRED_LINK_EN = "Link is expired"
+EXPIRED_LINK_DE = "Link abgelaufen"
+
